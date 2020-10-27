bbaef4563680ae3b1df96ce6b37a7cad9f61e11e
Raza Haider <raxa.hai@gmail.com>
2020-10-27 20:27:02 +0500

    Added isEmpty/isFull

--- stack.cpp
+++ stack.cpp
@@ -6,45 +6,72 @@ class Stack
 private:
     int top = -1;
     int *arr;
+    int size;
 public:
     Stack(int size);
     void push(int element);
     int pop();
     int peek(int position);
+    bool isEmpty();
+    bool isFull();
 };
 
 Stack::Stack(int size)
 {
     arr = new int[size];
+    this->size = size;
 }
 
 void Stack::push(int element){
-    top = top + 1;
-    arr[top] = element;
+    if (!isFull())   
+    {
+        top = top + 1;
+        arr[top] = element;
+        return;
+    }
+    cout<<"Stack Overflow->"<<element<<" Not added"<<endl;
 };
 
 int Stack::pop(){
-    return arr[top--];
+    if (!isEmpty())
+    {
+        return arr[top--];
+    }
+    cout<<"Stack Underflow"<<endl;
+    return 0;
 }
 
 int Stack::peek(int position){
     return arr[top-position+1];
 };
 
+bool Stack::isEmpty(){
+    if (top==-1)
+    {
+        return true;
+    }
+    return false;
+    
+};
+
+bool Stack::isFull(){
+    if (top==size-1)
+    {
+        return true;
+    }
+    return false;
+}
+
 int main()
 {
-    int size = 80;
+    int size = 50;
     Stack s1(size);
     s1.push(5);
     s1.push(6);
     s1.push(7);
     s1.push(8);
     s1.push(9);
-
+    s1.push(19);
+    s1.push(29);
     s1.pop();
-    s1.pop();
-    for (int i = 1; i < 4; i++)
-    {
-        cout<<s1.peek(i)<<endl;
-    }
 }
