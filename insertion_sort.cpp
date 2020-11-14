11a1a6a2dac988f5b1c8e03eaa4400cec8de6040
Raza Haider <raxa.hai@gmail.com>
2020-11-14 18:18:27 +0500

    refactored line#10

--- insertion_sort.cpp
+++ insertion_sort.cpp
@@ -2,18 +2,18 @@
 using namespace std;
 
 void insertion_sort(int *arr,int n){
-    int temp;
+    int temp,element;
     for (int i = 1; i < n; i++)
     {
-        for (int j = 0; j < i; j++)
+        element = arr[i];
+        int j = i-1;
+        while (element < arr[j] && j>=0)
         {
-            if (arr[i]<arr[j])
-            {
-                temp = arr[i];
-                arr[i] = arr[j];
-                arr[j] = temp;
-            }
+            cout<<"enter"<<endl;
+            arr[j+1] = arr[j];
+            j--;
         }
+        arr[j+1] = element;
     }
 }
 
@@ -26,10 +26,10 @@ void printArray(int *arr,int n){
 
 int main()
 {
-    // case-1 with no sorted array
+    // case-1 with no sorted array taking big-oh(n^2)
     // int arr[] = {5,4,3,2,1};
 
-    // case-2 with sorted array taking Big-oh(n^2)
+    // case-2 with sorted array taking Big-oh(n)
     int arr[] = {1,2,3,4,5};
     int n = sizeof(arr)/sizeof(int);
     cout<<"sorted array using insertion sort is:"<<endl;
